a4a7ad8af6c055084b6b39921cf7561e470aeedf
Chodutobixyz <mmalik91309@gmail.com>
2024-09-04 00:17:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7289819098:AAHjRCdgVIClGZ_I9h1kBo-Hncu7or0JKDQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("OBITOxVM")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1655261425")
 
 
 
